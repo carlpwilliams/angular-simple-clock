3ee046dbdd97085f68b6167bf5f3373cffc657aa
Carl Williams <carl@carlpwilliams.com>
2015-05-06 14:14:49 +0100

    Added demo, refactored into viewModel

---
+++ src/clock.controller.js
@@ -0,0 +1,23 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('clock.controller', []);
+
+    function ClockController($interval) {
+        var vm = this;
+        vm.model={};
+
+        vm.model.timeNow = Date.now();
+        vm.model.tickInterval = 1000;
+
+        vm.tick = function() {
+            vm.model.timeNow = Date.now();
+        };
+
+        $interval(vm.tick, vm.model.tickInterval);
+    }
+
+    angular
+        .module('clock.controller')
+        .controller('ClockController', [ '$interval', ClockController]);
+})();
---
+++ src/clock.js
@@ -0,0 +1,19 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('components.clock', ['clock.controller']);
+
+    function clock() {
+        return {
+            controller: 'ClockController',
+            controllerAs: 'clock',
+            replace: true,
+            restrict: 'AE',
+            template: '<time class="clock">{{ clock.model.timeNow  | date: "HH:mm:ss "}}</time>'
+        };
+    }
+
+    angular
+        .module('components.clock')
+        .directive('clock', clock);
+})();
